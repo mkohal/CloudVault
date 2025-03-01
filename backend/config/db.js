88d699b977249b3f248bafc7a122a454da6ba4f6
Maheshwar Kohal <maheshwarkohal8@gmail.com>
2025-03-01 18:57:22 +0530

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 
 let dbConnected = false; // Store connection status
 
-function connectToDB() {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-      console.log("Connected to DB");
-      dbConnected = true; // Update status on success
-    })
-    .catch((err) => {
-      console.error("DB Connection Error:", err);
-      dbConnected = false; // Update status on failure
-    });
+async function connectToDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to DB");
+    dbConnected = true; // Update status on success
+  } catch (err) {
+    console.error("DB Connection Error:", err);
+    dbConnected = false; // Update status on failure
+  }
 }
 
 function getDBStatus() {
