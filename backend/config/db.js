0438ff060600d576ad1512a6e8703c3a5449136f
Maheshwar Kohal <maheshwarkohal8@gmail.com>
2025-03-01 18:13:53 +0530

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 function connectToDB(){
     mongoose.connect(process.env.MONGO_URI).then(()=>{
         console.log("Connected to DB")
+        const value = "Connected to DB";
+        return value;
     })
 }
 
