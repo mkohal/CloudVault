2c6488bff80a4eea0d342aa4d38075a0b7de505b
Maheshwar Kohal <maheshwarkohal8@gmail.com>
2025-03-05 11:01:09 +0530

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://cloud-vault-backend.vercel.app";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
